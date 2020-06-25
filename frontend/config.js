5d1be4e2701340481d6f65270b3693282c831d7c
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2020-06-25 16:13:54 -0500

    frontend yoga

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://sick-fits-prod-3981713ddd.herokuapp.com/sick-fits-prod/prod`;
+export const prodEndpoint = `https://wbsickfits-yoga-prod.herokuapp.com/`;
 export const perPage = 4;
