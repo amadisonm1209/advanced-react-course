a7157b406e1ccf8cb2d841f9f9ccf4b62c572b26
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2020-06-25 15:50:53 -0500

    token issue

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://sick-fits-prod-3981713ddd.herokuapp.com/sick-fits-prod/prod`;
+export const prodEndpoint = `https://sick-fits-prod-3981713ddd.herokuapp.com/sick-fits-prod/prod?headers={"Authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTMyMDM4NzUsIm5iZiI6MTU5MzExNzQ3NX0.iQzCx0mV4-_r_dUisGWLwUz9XC-jvI6eDIe_v6iUeBE"}`;
 export const perPage = 4;
